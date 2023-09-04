2a44b2ee5ba8b2c44a71b2edc51632e8b7ac343d
sankar89 <sankarmanoharan89@gmail.com>
2023-09-04 21:39:18 +0530

    Bar chart added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Plot from 'react-plotly.js';
 
 
 function App() {
-  const data = [{
+  const pieChartData = [{
     type: "pie",
     values: [2, 3, 4, 4],
     labels: ["Wages", "Operating expenses", "Cost of sales", "Insurance"],
@@ -16,11 +16,33 @@ function App() {
     width: 1000
   }];
   
+  var trace1 = {
+    x: ['giraffes', 'orangutans', 'monkeys'],
+    y: [20, 14, 23],
+    name: 'SF Zoo',
+    type: 'bar'
+  };
+  
+  var trace2 = {
+    x: ['giraffes', 'orangutans', 'monkeys'],
+    y: [12, 18, 29],
+    name: 'LA Zoo',
+    type: 'bar'
+  };
+  const data = [trace1, trace2];
+  const barLayout = {barmode: 'group'};
+
   return (
     <div className="App">
+      <h1>Pie Chart</h1>
+      <Plot
+        data={pieChartData}
+        layout={layout}
+      />
+      <h1>Bar Chart</h1>
       <Plot
         data={data}
-        layout={layout}
+        layout={barLayout}
       />
     </div>
   );
